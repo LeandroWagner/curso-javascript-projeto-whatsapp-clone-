4618f280e0dfb6195eab4aefa29b5de54a1802ab
Leandro Wagner <lsw_wagner@yahoo.com.br>
2022-02-12 14:35:11 -0300

    transformando em camelcase os ids html

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,20 @@
+class WhatsAppController {
+
+    constructor() {
+        console.log('WhatsAppController OK')
+
+        //carrega os elementos
+        this.loadElements()
+
+    }
+
+    loadElements() {
+
+        this.el = {}
+
+        document.querySelectorAll('[id]').forEach(element=>{
+
+            this.el[Format.getCamelCase(element.id)] = element;
+        });
+    }
+}
