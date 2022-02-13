b62bdf394afa99f0a452100ee22b6de2f0fb0e5d
Leandro Wagner <lsw_wagner@yahoo.com.br>
2022-02-12 23:26:38 -0300

    adicionando prototype

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,6 +3,9 @@ class WhatsAppController {
     constructor() {
         console.log('WhatsAppController OK')
 
+        //prototype
+        this.elementsPrototype()
+
         //carrega os elementos
         this.loadElements()
 
@@ -17,4 +20,47 @@ class WhatsAppController {
             this.el[Format.getCamelCase(element.id)] = element;
         });
     }
+
+    //falar qual classe
+    elementsPrototype() {
+
+        Element.prototype.hide = function() {
+
+            this.style.display = 'none';
+        }
+
+        Element.prototype.show = function() {
+
+            this.style.display = 'block';
+        }
+
+        Element.prototype.toggle = function() {
+
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none'
+            
+        }
+
+        Element.prototype.on = function(events, fn) {
+
+            events.split('').forEach(event=>{
+
+                this.addEventListener(event, fn);
+
+            });
+            
+        }
+
+        Element.prototype.css = function(styles) {
+
+           for(let name in styles) {
+                this.style[name] = styles[name]
+
+           }
+            
+        }
+
+
+
+    }
+
 }
