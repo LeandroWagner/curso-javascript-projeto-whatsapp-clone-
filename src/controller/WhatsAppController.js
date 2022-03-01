d5952658ffd9200ab296d80211e06f2d75c3011f
Leandro Wagner <lsw_wagner@yahoo.com.br>
2022-03-01 20:48:38 -0300

    identifica o imojis no click

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -289,7 +289,63 @@ class WhatsAppController {
 
             this.closeRecordMicrophone();
         });
-    }   
+        //Emojis
+
+        this.el.btnEmojis.on('click', e=>{
+
+            this.el.panelEmojis.togleClass('open')
+
+        })
+
+        this.el.panelEmojis.querySelectosAll('.emojik').forech(emoji=>{
+
+            emojis.on('click', e=>{
+
+                console.log(emoji.dataset.unicode)
+            })
+        })
+
+
+
+        //inputs text
+        this.el.inputText.on('keypress', e=>{
+
+            if (e.key === 'Enter' && !e.ctrkey) {
+
+                e.preventDefault()
+                this.el.btnSend.click()
+
+            }
+
+        })
+
+        //inputs text
+        this.el.inputText.on('keyup', e=>{
+
+            //se tiver alguma coisa preenchida
+            if (this.el.inputText.innerHTML.length) {
+
+                this.el.inputPlaceholder.hide()
+                this.el.btnSendMicrophone.hide()
+                this.el.btnSend.show()
+
+            } else {
+
+                this.el.inputPlaceholder.show()
+                this.el.btnSendMicrophone.show()
+                this.el.btnSend.hide()
+            }
+
+        })
+    
+        this.el.btnSend.on('click', e=>{
+
+            console.log(this.el.inputText.innerHTML)
+        })
+    } 
+    
+    
+
     //timer da gravação.
     startRecordMicrophoneTime()
     {
