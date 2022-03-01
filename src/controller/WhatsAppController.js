599394049f3121c9002523252cede65d31d1b99e
Leandro Wagner <lsw_wagner@yahoo.com.br>
2022-03-01 20:11:42 -0300

    adicionada o microphone de gravacao

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -270,7 +270,43 @@ class WhatsAppController {
             this.el.modalContacts.hide()
         
         })
+
+        this.el.btnSendMicrophone.on('click', e=>{
+
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.el.startRecordMicrophoneTime();
+
+        });
+
+        this.el.btnCancelMicrophone.on('click', e=>{
+
+            this.closeRecordMicrophone();
+
+        });
+
+        this.el.btnFinishMicrophone.on('click', e=>{
+
+            this.closeRecordMicrophone();
+        });
     }   
+    //timer da gravação.
+    startRecordMicrophoneTime()
+    {
+        let start = Date.now()
+        this._recordMicrophoneInterval = setInterval(()=>{
+            this.el.recordMicropneTimer.innerHTML =(DataTransfer.now() - start);
+        },100)
+    }
+
+
+    closeRecordMicrophone() 
+    {
+       this.el.recordMicrophone.hide()
+       this.el.btnSendMicrophone.show()
+       clearInterval(this._recordMicrophoneInterval)
+    
+    }
 
     closeAllMainPanel() {
 
